31d61d7ecb352cfb9618775018ac770046fe487b
0xSiddhant <contactsiddhant2155@gmail.com>
2024-01-05 13:27:23 +0530

    Current date and time  added on stmt

--- Sources/SwiftLogger/Log.swift
+++ Sources/SwiftLogger/Log.swift
@@ -10,17 +10,17 @@ import Foundation
 enum Log {
     enum LogLevel {
         case info,
-        warning,
-        error
+             warning,
+             error
         
         fileprivate var prefix: String {
             switch self {
             case .info:
-                return "INFO"
+                return "ℹ️"
             case .warning:
-                return "WARN ⚠️"
+                return "⚠️"
             case .error:
-                return "ALERT ❌"
+                return "❌"
             }
         }
     }
@@ -31,15 +31,24 @@ enum Log {
         let line: Int
         
         var description: String {
-            return "\((file as NSString).lastPathComponent):\(line) \(function)"
+            return "[\((file as NSString).lastPathComponent):\(line) \(function)] "
         }
     }
     
+    static fileprivate var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        formatter.locale = Locale.current
+        formatter.timeZone = TimeZone.current
+        return formatter
+    }
+    
     fileprivate static func handleLog(level: LogLevel, str: String, shouldLogContext: Bool, context: Context) {
-        var logComponents = ["[\(level.prefix)]", " -> \(str)"]
+        var logComponents = [Date().toString(), "[\(level.prefix)]"]
         if shouldLogContext {
             logComponents.insert("\(context.description)", at: 1)
         }
+        logComponents.append(" -> \(str)")
         
         #if DEBUG
         print(logComponents.joined(separator: " "))
@@ -49,5 +58,11 @@ enum Log {
     static public func log(_ str: StaticString, logLevel: Log.LogLevel, shouldLogContext: Bool , file: String, function: String, line: Int) {
         let context = Context(file: file, function: function, line: line)
         Log.handleLog(level: logLevel, str: str.description, shouldLogContext: shouldLogContext, context: context)
+    }
+}
+
+internal extension Date {
+    func toString() -> String {
+        return Log.dateFormatter.string(from: self as Date)
     }
 }
